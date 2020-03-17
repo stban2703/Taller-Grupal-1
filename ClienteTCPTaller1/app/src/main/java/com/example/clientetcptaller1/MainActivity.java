5ac5f7409421efe6b950606d98d8f0379428287f
Isabella Linares <isabellalinarescamacho0517@gmail.com>
2020-03-17 15:23:07 -0500

    se creó la interfaz del control en android studio

---
+++ ClienteTCPTaller1/app/src/main/java/com/example/clientetcptaller1/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.clientetcptaller1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    //Button izquierdabtn;
+    //Button derechabtn;
+    //Button poderbtn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //izquierdabtn.findViewById(R.id.izquierdabtn);
+        //derechabtn.findViewById(R.id.derechabtn);
+        //poderbtn.findViewById(R.id.poderbtn);
+
+
+    }
+}
