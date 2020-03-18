2ad801821e966f037380ebc03e9192545a06ccac
stban2703 <s.t.ban1401@gmail.com>
2020-03-17 19:39:49 -0500

    La app se ejecuta en modo inmersivo y solo en landscape

--- ClienteTCPTaller1/app/src/main/java/com/example/clientetcptaller1/MainActivity.java
+++ ClienteTCPTaller1/app/src/main/java/com/example/clientetcptaller1/MainActivity.java
@@ -3,25 +3,42 @@ package com.example.clientetcptaller1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
     ComunicacionTCP comm;
-    //ImageButton izquierdabtn;
-    //ImageButton derechabtn;
-    //ImageButton poderbtn;
+    private ImageButton izquierdabtn;
+    private ImageButton derechabtn;
+    private ImageButton poderbtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        //izquierdabtn.findViewById(R.id.izquierdabtn);
-        //derechabtn.findViewById(R.id.derechabtn);
-        //poderbtn.findViewById(R.id.poderbtn);
-
         comm=new ComunicacionTCP(this);
         comm.solicitarConexion();
 
+        izquierdabtn = findViewById(R.id.izquierdabtn);
+        derechabtn = findViewById(R.id.derechabtn);
+        poderbtn = findViewById(R.id.poderbtn);
+
     }
+
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        //This is used to hide/show 'Status Bar' & 'System Bar'. Swip bar to get it as visible.
+        View decorView = getWindow().getDecorView();
+        if (hasFocus) {
+            decorView.setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        }
+    }
+
 }
